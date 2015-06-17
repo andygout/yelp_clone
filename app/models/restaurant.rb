44c833213c44986846db1f44a1432c21aef909a7
andygout <andygout@hotmail.co.uk>
2015-06-17 17:47:42 +0100

    Adds: build_with_user method to restaurant.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,12 @@
 class Restaurant < ActiveRecord::Base
 
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy do
+    def build_with_user(attributes = {}, user)
+      attributes[:user] ||= user
+      build(attributes)
+    end
+  end
+
   validates :name, length: {minimum: 3}, uniqueness: true
 
 end
