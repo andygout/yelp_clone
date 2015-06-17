3db29b594268dc410b66ad1ef755ba4a3779abe9
Rocco <roccorighi@hotmail.com>
2015-06-17 18:12:48 +0100

    fixes merge problems

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,8 @@
 class Restaurant < ActiveRecord::Base
 
-  has_many :reviews, dependent: :destroy
-  validates :name, length: {minimum: 3}, uniqueness: true
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :restrict_with_exception, dependent: :destroy
 
-  def build_review(current_user, review_params)
-    review_foreign_keys = {:restaurant => self, :user => current_user}
-    review = Review.new(review_params.merge(review_foreign_keys))
-  end
+  validates :name, length: {minimum: 3}, uniqueness: true
 end
