263d07f9aea37088090dddc8b5dee8224691020a
andygout <andygout@hotmail.co.uk>
2015-06-18 11:29:36 +0100

    Adds: Star rating methods test passing

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,7 +1,8 @@
 module ReviewsHelper
 
   def star_rating rating
-    return rating unless rating.is_a?(Fixnum)
+    return rating unless rating.respond_to?(:round)
+    rating = rating.round
     remainder = (5 - rating)
     '★' * rating + '☆' * remainder
   end
