d8626d9fad88a54f76216ea13360d87504585673
andygout <andygout@hotmail.co.uk>
2015-06-18 15:39:45 +0100

    Adds: Endorsements functional

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -10,7 +10,7 @@ feature 'endorsing reviews' do
 
   scenario 'a user can endorse a review, which updates the review endorsement count', js: true do
     sign_up('test@test.com', 'testpassword')
-    click_link 'Endorse KFC'
+    click_link 'Endorse'
     expect(page).to have_content('1 endorsement')
   end
 
